cf9e077034585c543ef57f5182ed4a0054fdc5c9
huzaifa250 <huz.dark1@gmail.com>
2024-10-16 09:51:59 +0000

    edit ts1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,11 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """return 10 random list number generated asynchronously"""
     res = [float(ise) async for ise in async_generator()]
-    # Checking the function annotations
-    print(async_comprehension.__annotations__)
     return res
+    # Checking if the return value is annotated
+    if 'return' in async_comprehension.__annotations__:
+        print(f"The return type is annotated as: "
+              f"{async_comprehension.__annotations__['return']}"
+              )
+    else:
+        print("The return type is not annotated.")
