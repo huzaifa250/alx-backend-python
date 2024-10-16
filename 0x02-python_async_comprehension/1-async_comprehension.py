0344e096e2cda886896300cd535e3b6bb8c3a368
huzaifa250 <huz.dark1@gmail.com>
2024-10-16 07:36:29 +0000

    edit task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" Module to returns 10 random numbers using async comprehension"""
+""" Module to defines an asynchronous function"""
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[int]:
-    res = [ise async for ise in async_generator()]
-    return res
+    """return 10 random list number generated asynchronously"""
+    return [ise async for ise in async_generator()]
